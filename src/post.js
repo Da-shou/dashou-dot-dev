885a00136300667e1e3a462c99dd6f99f3bd92c3
Da-shou <fazlijaylli@gmail.com>
2025-11-16 11:53:23 +0100

    Fetched test data from database
    Added some data to the database and then used the sqlite3 node package to fetch that data from the database, and finally displayed it on the website.

--- src/post.js
+++ src/post.js
@@ -1,12 +1,22 @@
 'use strict';
 
-// Fake posts database
+const sqlite3 = require('sqlite3').verbose();
+const path = require('node:path');
 
-var posts = [
-  { title: 'Foo', body: 'some foo bar' },
-  { title: 'Foo bar', body: 'more foo bar' },
-  { title: 'Foo bar baz', body: 'more foo bar baz' }
-];
+const db = new sqlite3.Database(path.join(__dirname,"../database/blogdata.db"), sqlite3.OPEN_READWRITE, (err) => {
+	if (err) console.error(err.message); 
+	else console.log("Successfully connected to the database");
+});
+
+let posts;
+db.all("SELECT id_post, title, content FROM post", (err, rows) => {
+	if (err) throw err; 
+	
+	posts = rows;	
+	rows.forEach((row) => {
+		console.log(row.id_post + ': ' + row.title);
+	});
+});
 
 exports.list = function(_, res){
   res.render('posts', { title: 'Posts', posts: posts });
