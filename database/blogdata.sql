d8b0da64e73269215154e55abdd22a3f12a47f95
FazlijaYlli <ylli.fazlija@heig-vd.ch>
2025-11-18 10:42:06 +0100

    Added about page and responsive CSS
    An about page has been added which contain a small amount of information about me. Most importantly, basic responsive CSS has been added to support mobile devices. It's not the prettiest, but it's usable and readable and that's what counts.

--- database/blogdata.sql
+++ database/blogdata.sql
@@ -1,24 +1,26 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "category" (
 	"id_category"	INTEGER NOT NULL,
-	"name"	VARCHAR(64) NOT NULL,
+	"name"	TEXT NOT NULL,
 	PRIMARY KEY("id_category" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "comment" (
 	"id_comment"	INTEGER NOT NULL,
-	"author"	VARCHAR(32) NOT NULL,
-	"content"	VARCHAR(512) NOT NULL,
-	"timestamp"	TIMESTAMP NOT NULL,
+	"author"	TEXT NOT NULL DEFAULT 'Author',
+	"content"	TEXT NOT NULL DEFAULT 'Content',
+	"timestamp"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"id_post"	INT NOT NULL,
 	PRIMARY KEY("id_comment" AUTOINCREMENT),
 	CONSTRAINT "fk_comment_post" FOREIGN KEY("id_post") REFERENCES "post"("id_post") ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 CREATE TABLE IF NOT EXISTS "post" (
 	"id_post"	INTEGER NOT NULL,
-	"title"	TEXT NOT NULL,
-	"content"	LONGTEXT NOT NULL,
-	"timestamp"	TIMESTAMP NOT NULL,
-	"id_category"	INT NOT NULL,
+	"title"	TEXT NOT NULL DEFAULT 'Default title',
+	"content"	TEXT NOT NULL DEFAULT 'Default description',
+	"timestamp"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"id_category"	INTEGER NOT NULL DEFAULT 1,
+	"abstract"	TEXT NOT NULL DEFAULT 'Default abstract',
+	"thumbnail"	TEXT,
 	PRIMARY KEY("id_post" AUTOINCREMENT),
 	CONSTRAINT "fk_post_category" FOREIGN KEY("id_category") REFERENCES "category"("id_category") ON DELETE NO ACTION ON UPDATE NO ACTION
 );
@@ -31,20 +33,9 @@ CREATE TABLE IF NOT EXISTS "post_has_tag" (
 );
 CREATE TABLE IF NOT EXISTS "tag" (
 	"id_tag"	INTEGER NOT NULL,
-	"name"	VARCHAR(32) NOT NULL,
+	"name"	TEXT NOT NULL,
 	PRIMARY KEY("id_tag" AUTOINCREMENT)
 );
-INSERT INTO "category" ("id_category","name") VALUES (1,'Programming'),
- (2,'Music'),
- (3,'');
-INSERT INTO "post" ("id_post","title","content","timestamp","id_category") VALUES (1,'My first post','Welcome to my first blog post. If you can see this text, your''re pretty early !','2025-11-13 14:47:40',1);
-INSERT INTO "post_has_tag" ("id_post","id_tag") VALUES (1,3),
- (1,4);
-INSERT INTO "tag" ("id_tag","name") VALUES (1,'html'),
- (2,'css'),
- (3,'c'),
- (4,'glfw'),
- (5,'cpp');
 CREATE INDEX IF NOT EXISTS "fk_comment_post_idx" ON "comment" (
 	"id_post"
 );
--- src/post.js
+++ src/post.js
@@ -8,10 +8,19 @@ const db = new sqlite3.Database(path.join(__dirname,"../database/blogdata.db"), 
 	else console.log("✅ Successfully connected to the database !");
 });
 
+const max_abstract_length = 250;
+
 exports.list = function(_, res){
-	db.all("SELECT id_post, title, content, timestamp FROM post", (err, rows) => {
+	db.all("SELECT id_post AS id, id_category AS category, title, abstract, content, thumbnail, timestamp FROM post", (err, rows) => {
 		if (err) throw err;	
 		const posts = rows;
-  		res.render('posts', { title: `Browse ${posts.length} posts...`, posts: posts });
+
+        rows.forEach(post => {
+            if (post.abstract.length > max_abstract_length) {
+                post.abstract = post.abstract.substring(0, max_abstract_length + 1) + "...";
+            }
+        })
+
+  		res.render('posts/posts.ejs', { title: `Browse ${posts.length} posts...`, posts: posts });
 	});
 };
